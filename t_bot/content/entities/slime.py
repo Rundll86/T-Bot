6277a261e53ab3670756e7128d5db6f7104351d8
陨落基围虾 <3161880837@qq.com>
2026-06-26 19:56:07 +0800

    更新史莱姆实体速度：将速度设置为随机值，以增强游戏的不可预测性

--- t_bot/content/entities/slime.py
+++ t_bot/content/entities/slime.py
@@ -1,10 +1,12 @@
+import random
+
 from t_bot.engine.world.target import BaseEntity
 
 
 class SlimeEntity(BaseEntity):
     def __init__(self) -> None:
         super().__init__("史莱姆", 100, "史")
-        self.speed = 0.5
+        self.speed = random.uniform(0.1, 0.6)
 
     def register_events(self):
         super().register_events()
